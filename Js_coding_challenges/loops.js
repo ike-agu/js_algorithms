941cd9ffba14c9c52b47bf2c8951cb5c5df16416
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-01-19 13:53:09 +0000

    execise on loops

---
+++ Js_coding_challenges/loops.js
@@ -0,0 +1,5 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for(let i = 0; i < vacationSpots.length[i]; i++ ){
+console.log(console.log(`I would love to visit ${vacationSpots.length[i]}`))
+}
