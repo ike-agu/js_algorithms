d414159704c8d1989fbad18bd27d00191eaba740
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-01-20 12:07:13 +0000

    created array method files with some algorithm challenges

---
+++ Js_coding_challenges/array_methods.js
@@ -0,0 +1,76 @@
+// task 1 returning 1st letter of each element in array with .map()
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale',
+ 'octopus', 'rabbit', 'lion', 'dog'];
+
+const secretMessage = animals.map(function(element){
+return element[0]
+});
+console.log(secretMessage.join(''));
+
+// task 1A returning results of element in array divided by 100 using .map()
+
+// arrow function
+const bigNumbers = [100, 200, 300, 400, 500];
+const smallNumbers = bigNumbers.map(num => num/100 );
+console.log(smallNumbers)
+
+
+// Task 2
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+const smallNumbers = randomNumbers.filter(num => num < 250)
+
+// TASK 2A filtering words sizes are >7 with .filter()
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+const longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+
+// TASK 3  finding the index of an element in an array using .findIndex()
+
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey',
+'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex( animal => animal === 'elephant' );
+
+const startsWithS = animals.findIndex( animal => animal[0] === 's' );
+
+console.log(foundAnimal )
+
+
+// TASK 4 summing the words in array with .reduce()
+
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
+}, 10);
+console.log(newSum)
+
+// TASK 5 returning some words < 6
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+
+
+
+const interestingWords = words.filter( word => word.length > 5 );
+
+ console.log(interestingWords.every((word) => { return word.length > 5} ));
+
+//  TASK 6 reversing a string
+
+function reverseString(str) {
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    return joinArray; // "olleh"
+}
+
+reverseString("hello");
--- Js_coding_challenges/loops.js
+++ Js_coding_challenges/loops.js
@@ -3,3 +3,49 @@ const vacationSpots = ['Bali', 'Paris', 'Tulum'];
 for(let i = 0; i < vacationSpots.length[i]; i++ ){
 console.log(console.log(`I would love to visit ${vacationSpots.length[i]}`))
 }
+
+
+//  NESTED LOOPS
+
+let bobsFollowers = ['ike','joe','anna','mike']
+let tinasFollowers = ['John','anna','mike']
+let mutualFollowers = []
+
+for(let i = 0; i < bobsFollowers.length; i++ ){
+  for(let j = 0; j < tinasFollowers.length; j++){
+    if(bobsFollowers[i] === tinasFollowers[j] ){
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  };
+};
+
+
+// WHILE LOOP 1
+
+let counterTwo = 1;
+while (counterTwo < 4) {
+  console.log(counterTwo);
+  counterTwo++;
+}
+
+// WHILE LOOP 2
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+let currentCard;
+
+while(currentCard != 'spade'){
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard)
+};
+
+
+// do...while loop
+
+let cupsOfSugarNeeded = 0;
+let cupsAdded = 0
+
+do {
+  cupsAdded++;
+  console.log(cupsAdded + ' cup of sugar was added' );
+}while(cupsAdded <  cupsOfSugarNeeded)
