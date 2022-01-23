54cc072079ddd109e41208562d00995e94784746
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-01-23 23:59:39 +0000

    solving take a walk challenge

--- Js_coding_challenges/array_methods.js
+++ Js_coding_challenges/array_methods.js
@@ -86,3 +86,38 @@ function capitalizeName(name){
 };
 
 console.log(capitalizeName('ikenna fraNklin johN mike'));
+
+// task 8 take a walk challenge
+
+public class TakeATenMinuteWalk {
+
+    public static boolean isValid(char[] walk) {
+        if (walk.length != 10) {
+            return false;
+        }
+
+        int x = 0;
+        int y = 0;
+
+        for (Character c : walk) {
+            switch (c) {
+                case 'n':
+                    y++;
+                    break;
+                case 's':
+                    y--;
+                    break;
+                case 'e':
+                    x--;
+                    break;
+                case 'w':
+                    x++;
+                    break;
+                default :
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
